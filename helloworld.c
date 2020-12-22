f64c3abe96d5ef45cbd5a3631c1300527337fdee
sparklers power coder <59435694+sudha2327@users.noreply.github.com>
2020-12-22 09:27:45 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+# hello-world--c-
+hacker rank c program solution
+
+#include<stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include<conio.h>
+int main(){
+	char s[100];
+	char c;
+	scanf("%[^\n]%*c", &s);
+	printf("Hello,World\n%s",s);
+return 0;
+	
+}
